26ee6a880f481a4bdc03da90ccae36357444bb70
Armaan0805 <armaanfatima18@gmail.com>
2025-05-30 20:21:39 +0530

    cleaned original code

--- functions.py
+++ functions.py
@@ -2,12 +2,10 @@ FILEPATH ="todos.txt"
 
 
 def get_todos(filepath=FILEPATH):
-    """Read the text file and return the list of to-do items, stripped and cleaned."""
-    with open(filepath, "r") as file:
-        todos = file.readlines()
-    # Strip each line and remove empty ones
-    return [todo.strip() for todo in todos if todo.strip()]
-
+    """Read the text file and return the list to-do items."""
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
 
 
 def  write_todos(todos_arg,filepath=FILEPATH):
